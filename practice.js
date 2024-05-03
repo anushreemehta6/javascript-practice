b457d030e04c280675beb5198ea008c88b64b665
anushree mehta <anushreemehta6@gmail.com>
2024-05-03 23:31:39 +0530

    new

--- practice.js
+++ practice.js
@@ -1,1 +1,27 @@
-console.log("Hello Js!!")
+console.log("Hello Js!!");
+const anushree={
+    name: "anushree mehta" ,
+    age: 18,
+    birthday: "8th may 2005",
+
+}
+
+console.log(anushree.birthday);
+
+// let colour= prompt("enter a colour");
+// console.log(colour);
+// colour= "black"? console.log("the colour is black"): console.log("the colour is not black ")
+
+let color;
+let mode;
+mode = prompt("enter the mode")
+ 
+if(mode=="dark mode"){
+    color = "black"
+    console.log(color);
+}
+else{
+    color="white"
+    console.log(color);
+}
+
