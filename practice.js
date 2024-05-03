35c7dea1d780f7715f74947f5515a6fb268462b5
anushree mehta <anushreemehta6@gmail.com>
2024-05-03 23:51:38 +0530

    strings

--- practice.js
+++ practice.js
@@ -1,27 +1,45 @@
 console.log("Hello Js!!");
-const anushree={
+let obj ={
     name: "anushree mehta" ,
-    age: 18,
+    age: 19,
     birthday: "8th may 2005",
 
 }
+console.log(`the name of ${obj.name} is ${obj.age}`);
+ 
 
-console.log(anushree.birthday);
+console.log(obj.birthday);
 
 // let colour= prompt("enter a colour");
 // console.log(colour);
 // colour= "black"? console.log("the colour is black"): console.log("the colour is not black ")
 
-let color;
-let mode;
-mode = prompt("enter the mode")
+// let color;
+// let mode;
+// mode = prompt("enter the mode")
  
-if(mode=="dark mode"){
-    color = "black"
-    console.log(color);
-}
-else{
-    color="white"
-    console.log(color);
+// if(mode=="dark mode"){
+//     color = "black"
+//     console.log(color);
+// }
+// else{
+//     color="white"
+//     console.log(color);
+// }
+
+for(let i=1; i<=10;i++){
+    console.log(i)
 }
 
+let str="anushree mehta ";
+
+for(let val of str){
+ console.log(val)
+}
+
+ let a= str.length;
+ console.log(a);
+
+
+
+ 
