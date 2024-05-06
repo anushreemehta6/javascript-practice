f5d3a1e0d7d2bd42da8ff0c7b41dab25828b7395
anushree mehta <anushreemehta6@gmail.com>
2024-05-06 22:33:07 +0530

    methods

--- practice.js
+++ practice.js
@@ -1,12 +1,10 @@
 console.log("Hello Js!!");
-let obj ={
-    name: "anushree mehta" ,
-    age: 19,
-    birthday: "8th may 2005",
-
-}
+let obj = {
+  name: "anushree mehta",
+  age: 19,
+  birthday: "8th may 2005",
+};
 console.log(`the name of ${obj.name} is ${obj.age}`);
- 
 
 console.log(obj.birthday);
 
@@ -17,7 +15,7 @@ console.log(obj.birthday);
 // let color;
 // let mode;
 // mode = prompt("enter the mode")
- 
+
 // if(mode=="dark mode"){
 //     color = "black"
 //     console.log(color);
@@ -27,29 +25,26 @@ console.log(obj.birthday);
 //     console.log(color);
 // }
 
-for(let i=1; i<=10;i++){
-    console.log(i)
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
 
-let str="anushree mehta ";
+let str = "anushree mehta ";
 
-for(let val of str){
- console.log(val)
+for (let val of str) {
+  console.log(val);
 }
 
- let a= str.length;
- console.log(a);
+let a = str.length;
+console.log(a);
 
-
-
-function myfunction(mes){
-    console.log(mes);
+function myfunction(mes) {
+  console.log(mes);
 }
- 
+
 myfunction("welcome to apna college");
 
-
-// function-> 2 numbers sum 
+// function-> 2 numbers sum
 
 // function sum(a,b){
 //     let add= a+b;
@@ -58,19 +53,41 @@ myfunction("welcome to apna college");
 
 // sum(5,6);
 
+const sum = (a, b) => {
+  let add = a + b;
+  console.log(add);
+};
 
-const sum = (a,b)=>{
-     let add= a+b;
-     console.log(add);
+sum(4, 4);
+
+const mul = (a, b) => {
+  let product = a * b;
+  console.log(product);
+};
+
+mul(4, 2);
+
+// create a function that takes strings as a argument and returns the vowels in the string
+
+function countvowel(str) {
+  let count = 0;
+  for (let char of str) {
+    if (
+      char === "A" ||
+      char === "E" ||
+      char === "I" ||
+      char === "O" ||
+      char === "U"
+    ) {
+      count = count + 1;
+    }
+  }
+  console.log(count);
 }
 
- sum(4,4);
+countvowel("NITI");
 
- const mul =(a,b) =>{
-    let product= a*b;
-    console.log(product);
- }
-
- mul(4,2)
-
- 
+let arr = [1, 2, 3, 4, 5];
+arr.forEach((element) => {
+  console.log(element * element);
+});
