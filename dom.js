693ac3b02a4f9f2354d41fcedb4acf3a941c50ff
anushree mehta <anushreemehta6@gmail.com>
2024-05-09 15:47:58 +0530

    dom2

--- dom.js
+++ dom.js
@@ -2,3 +2,11 @@
 //  console.dir(append)
 
 //  append.innerText ="hello javascript from apna college"
+
+let newbtn = document.createElement("button")
+newbtn.innerText="click me!!"
+console.log(newbtn)
+
+let div = document.querySelector("div")
+
+div.after(newbtn)
