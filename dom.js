cf05e5b3f4454f210289c2686f39e82588bb0bcc
anushree mehta <anushreemehta6@gmail.com>
2024-05-09 16:34:45 +0530

    dom complete

--- dom.js
+++ dom.js
@@ -3,10 +3,25 @@
 
 //  append.innerText ="hello javascript from apna college"
 
-let newbtn = document.createElement("button")
-newbtn.innerText="click me!!"
-console.log(newbtn)
+// let newbtn = document.createElement("button")
+// newbtn.innerText="click me!!"
+// console.log(newbtn)
 
-let div = document.querySelector("div")
+// let div = document.querySelector("div")
 
-div.after(newbtn)
+// div.after(newbtn)
+
+let newbtn = document.createElement("button");
+newbtn.innerText="click me!!";
+
+document.querySelector("body").prepend(newbtn);
+
+newbtn.style.backgroundColor="red"
+newbtn.style.color="white"
+
+
+//q2
+
+let para = document.querySelector("p")
+para.classList
+para.classList.add("newclass")
